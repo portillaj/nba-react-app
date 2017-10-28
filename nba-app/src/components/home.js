8b310ec3a491f8c61f257feb63701de78b57519b
Jason Portilla <jasonportilla@Jasons-MacBook-Pro.local>
2017-10-28 07:38:01 -0400

    finished most of the app and added react-reveal and some animation

---
+++ nba-app/src/components/home.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+//Components
+import Featured from './featured';
+import Subscriptions from './subscriptions';
+import Blocks from './blocks';
+
+const url_home = 'http://localhost:3004/home';
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            home: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch(url_home, {
+            method: 'GET'
+        })
+        .then(response => response.json())
+        .then(json => {
+            this.setState({
+                home: json
+            })
+        });
+    }
+
+    render() {
+        return(
+          <div>
+            <Featured slides={this.state.home.slider} />
+            <Subscriptions />
+            <Blocks blocks={this.state.home.blocks}/>
+          </div> 
+        );
+    }
+}
+
+export default Home;
